4f3662c8fa5c08c15bd342fb7099832b4ba9c32f
matthewtrahan <mtrahan920@sbcglobal.net>
2017-09-23 16:47:45 -0500

    added ai to choose an elegant phrase

--- marriottBot.py
+++ marriottBot.py
@@ -1,11 +1,14 @@
-import tweepy, time
+import tweepy, time, random
 from credentials import *
+from random import choice
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_SECRET)
 api = tweepy.API(auth)
 
-marriottTweet = "I love being a Marriott member! Marriott for life! #MembersGetIt #RewardsPoints #FindYourRoute #MRpoints"
+phrases = ["Allegedly", "Wubbalubbadubdub", "Schlim Schlam Schlippidy Doo", "I love Marriott", "Hook 'Em", "H-Town"]
+
+marriottTweet = random.choice(phrases) + " #MembersGetIt #RewardsPoints #FindYourRoute #MRpoints"
 
 api.update_status(marriottTweet)
 print(marriottTweet)
